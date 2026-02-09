eeb47925aabfc39a993e8e5f41a458d97ed2ba39
Debarati Basu-Nag <dbasunag@redhat.com>
2026-02-09 09:36:50 -0500

    update based on review comments
    Signed-off-by: Debarati Basu-Nag <dbasunag@redhat.com>

--- catalog/clients/python/odh_rules_catalog.mk
+++ catalog/clients/python/odh_rules_catalog.mk
@@ -1,6 +1,8 @@
 .PHONY: test-e2e-catalog
 test-e2e-catalog:
-	export CATALOG_URL="https://$$(kubectl get route -n rhoai-model-registries model-catalog-https -o 'jsonpath={.status.ingress[0].host}')/" && \
+	@echo "Running catalog tests..."
+	export MC_NAMESPACE=$$(kubectl get datasciencecluster default-dsc -o jsonpath='{.spec.components.modelregistry.registriesNamespace}') && \
+	export CATALOG_URL="https://$$(kubectl get route -n "$$MC_NAMESPACE" model-catalog-https -o 'jsonpath={.status.ingress[0].host}')/" && \
 	export AUTH_TOKEN=$$(kubectl config view --raw -o jsonpath="{.users[?(@.name==\"$$(kubectl config view -o jsonpath="{.contexts[?(@.name==\"$$(kubectl config current-context)\")].context.user}")\")].user.token}") && \
 	export VERIFY_SSL=False && \
 	export KIND_CLUSTER=False && \
